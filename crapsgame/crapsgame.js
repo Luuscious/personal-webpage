1ced5a0bb1c95010c32b5a1ed87434492ebb8ac3
Luuscious <bankoleyetunde1997@gmail.com>
2026-08-18 18:38:20 +0100

    Added variables to track user rounds and money

--- crapsgame/crapsgame.js
+++ crapsgame/crapsgame.js
@@ -13,6 +13,10 @@ const crapsStatsUsername = "craps-stats-username"
 const crapsStatsMoney = "craps-stats-money"
 const crapsStatsRounds = "craps-stats-rounds"
 
+//In-game variables
+let currentMoney = startingMoney
+let currentRounds = startingRounds
+
 function registerCrapsPlayer(){
 		crapsUsername = document.getElementById(crapsUsernameInput).value
 
@@ -37,8 +41,10 @@ function showMainGameSection(){
 
 function setUpFirstRound(){
             document.getElementById(crapsStatsUsername).innerHTML = crapsUsername
-            setMoney(startingMoney)
-            setRounds(startingRounds)
+            currentMoney = startingMoney
+            currentRounds = startingRounds
+            setMoney(currentMoney)
+            setRounds(currentRounds)
     }
 
 function setMoney(money){
